4f56a9302381f8b6a6410ba6cc9fa5cdcb2de2db
unknown <simeonolawale69@gmail.com>
2022-08-23 06:52:29 +0530

    secon section of lading page

--- src/Pages/Landing/Landing.js
+++ src/Pages/Landing/Landing.js
@@ -24,7 +24,7 @@ const Landing = () => {
                 <img src={foodPic} alt='food'/>
             </div>
         </div>
-        <div className='seconSection'>
+        <div className='secondSection'>
           <h2>Special Meals of the day!</h2>
           <p> Check our sepecials of the day and get discounts on all our meals <br/> and swift delivery to what ever location within Ilorin.</p>
         </div>
